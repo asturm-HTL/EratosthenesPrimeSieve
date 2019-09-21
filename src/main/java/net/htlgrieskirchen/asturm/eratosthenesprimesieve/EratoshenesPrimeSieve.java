17f7b48ff3aa0fd729f158223651799e61753e2a
Andreas Sturm <Andi_Sturm@gmx.at>
2019-09-21 10:49:07 +0200

    First Commit

---
+++ src/main/java/net/htlgrieskirchen/asturm/eratosthenesprimesieve/EratoshenesPrimeSieve.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgrieskirchen.asturm.eratosthenesprimesieve;
+
+/**
+ *
+ * @author asturm
+ */
+public class EratoshenesPrimeSieve {
+    
+}
