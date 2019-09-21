c97a830898b7acdb46543e86371d7fa91f876a45
Andreas Sturm <Andi_Sturm@gmx.at>
2019-09-21 11:00:47 +0200

    Interface and abstract methods (now saved)

--- src/main/java/net/htlgrieskirchen/asturm/eratosthenesprimesieve/EratoshenesPrimeSieve.java
+++ src/main/java/net/htlgrieskirchen/asturm/eratosthenesprimesieve/EratoshenesPrimeSieve.java
@@ -5,12 +5,37 @@
  */
 package net.htlgrieskirchen.asturm.eratosthenesprimesieve;
 
+import net.htlgrieskirchen.asturm.git.eratosthenesprimesieve.PrimeSieve;
+
 /**
  *
  * @author asturm
  */
-public class EratoshenesPrimeSieve 
+public class EratoshenesPrimeSieve implements PrimeSieve 
 {
+    //-----------------------Interface-----------------------------
+        public interface PrimeSieve
+        {
+            //Überprüft ob p eine Primzahl ist oder nicht.
+                public boolean isPrime(int p);
+            //Gibt alle Primzahlen bis zur Obergrenze auf der Konsole aus.
+                public void printPrimes();
+        }
+    //-------------------------------------------------------------
+
+    //---------------------abstract methods------------------------
+        @Override
+        public boolean isPrime(int p) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void printPrimes() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    //-------------------------------------------------------------
+        
+    
     
     
     
