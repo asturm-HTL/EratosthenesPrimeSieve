5eb333d21b7963ab6550f28602386f5f55d73e38
Andreas Sturm <Andi_Sturm@gmx.at>
2019-09-24 10:54:32 +0200

    Start Exercise 2

--- src/main/java/net/htlgrieskirchen/asturm/eratosthenesprimesieve/EratosthenesPrimeSieve.java
+++ src/main/java/net/htlgrieskirchen/asturm/eratosthenesprimesieve/EratosthenesPrimeSieve.java
@@ -60,7 +60,7 @@ public class EratosthenesPrimeSieve implements PrimeSieve
         public void printPrimes() 
         {
 
-            
+           
             
         }
     //-------------------------------------------------------------
